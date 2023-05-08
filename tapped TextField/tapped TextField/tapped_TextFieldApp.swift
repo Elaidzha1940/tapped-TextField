d42ce17616118a78464f7580107a36d1b2abf35c
Elaidzha1940 <eli080293@gmail.com>
2023-05-08 19:26:38 +0700

    tapped TextField

---
+++ tapped TextField/tapped TextField/tapped_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  tapped_TextFieldApp.swift
+//  tapped TextField
+//
+//  Created by Elaidzha Shchukin on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct tapped_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
