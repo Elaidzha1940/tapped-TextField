9d24fcca0f3ec220a49056469285f34d9758d1a3
Elaidzha1940 <eli080293@gmail.com>
2023-05-08 19:48:51 +0700

    tapped TextField + added a New File / SortView

--- tapped TextField/tapped TextField/ContentView.swift
+++ tapped TextField/tapped TextField/ContentView.swift
@@ -9,20 +9,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+enum SortEnum {
+    case priceincrease, descendingprice, byrating, bypromotions
+}
+
+
+struct SortView: View {
+    @State var is_selected: SortEnum = .priceincrease
+    
+    @Binding var showConfirmationDialog: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            Button {
+                self.is_selected = .priceincrease
+                didTap()
+            } label: {
+                getLabel(title: "По возрастанию цены", type: .priceincrease)
+            }
+            
+            Button {
+                self.is_selected = .descendingprice
+                didTap()
+            } label: {
+                getLabel(title: "По убыванию цены", type: .descendingprice)
+            }
+            
+            Button {
+                self.is_selected = .byrating
+                didTap()
+            } label: {
+                getLabel(title: "По рейтингу", type: .byrating)
+            }
+            
+            Button {
+                self.is_selected = .bypromotions
+                didTap()
+            } label: {
+                getLabel(title: "По акциям", type: .bypromotions)
+            }
         }
-        .padding()
+        .foregroundColor(.brown)
+        .background(
+            Color.blue
+        )
+        .cornerRadius(20)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SortView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SortView(showConfirmationDialog: .constant(false))
+    }
+}
+
+extension SortView {
+    @ViewBuilder
+    func getLabel(title: String, type: SortEnum) -> some View {
+        HStack {
+            Text(title)
+            Spacer()
+        }
+        .padding()
+        .background(is_selected == type ? Color.red : .clear)
+    }
+    
+    func didTap() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation {
+                showConfirmationDialog = false
+            }
+        }
     }
 }
--- tapped TextField/tapped TextField/tapped_TextFieldApp.swift
+++ tapped TextField/tapped TextField/tapped_TextFieldApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct tapped_TextFieldApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SortView(showConfirmationDialog: .constant(false))
         }
     }
 }
